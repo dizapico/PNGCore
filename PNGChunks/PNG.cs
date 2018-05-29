3cc53428fea4d7311b08986b812615f66aecdf1d
Diego Zapico Ferreiro <d.zapico@hotmail.com>
2018-05-29 11:51:56 +0200

    Remove personal openbadge on git

---
+++ PNGChunks/PNG.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PNGChunks
+{
+    public class PNG
+    {
+        private readonly byte[] _header = new byte[8];
+        private readonly IList<Chunk> _chunks;
+
+        public PNG(Stream ImageStream)
+        {
+            //Paso el stream a lista de chunks
+        }
+
+        private Chunk ChunkFromStream(Stream Stream)
+        {
+            byte[] length = ReadBytes(Stream, 4);
+            byte[] type = ReadBytes(Stream, 4);
+            Array.Reverse(length);
+            byte[] data = ReadBytes(Stream, BitConverter.ToInt32(length, 0));
+
+            //Jump CRC data
+            Stream.Seek(4, SeekOrigin.Current);
+
+            return new Chunk(type, data);
+        }
+
+        private byte[] ReadBytes(Stream stream, int n)
+        {
+            var buffer = new byte[n];
+            stream.Read(buffer, 0, n);
+            return buffer;
+        }
+
+    }
+}
